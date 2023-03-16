d5cf15da6695c01674129b8f4055cf8d5ba0ffe7
Tetiana Pesotska <verhulev81@gmail.com>
2023-03-17 00:26:28 +0200

    add js1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+console.log(buttonStartEl);
+console.log(buttonStopEl);
+console.log(bodyEl);
+
+buttonStartEl.addEventListener('click', onStart);
+buttonStopEl.addEventListener('click', onStop);
+
+let timerId = null;
+
+// function onStart(event) {
+//   //   console.log('click');
+//   timerId = setInterval(getRandomHexColor, 1000);
+// }
+
+function onStart(event) {
+  buttonStartEl.disabled = true;
+  buttonStopEl.disabled = false;
+
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop(event) {
+  buttonStartEl.disabled = false;
+  buttonStopEl.disabled = true;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
